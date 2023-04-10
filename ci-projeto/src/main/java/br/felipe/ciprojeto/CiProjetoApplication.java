500ea102da5b598657fbd0eacdd1997fcb404478
felipe-desktop <luiz.cdurante123@gmail.com>
2023-04-10 10:20:58 -0300

    first commit

---
+++ ci-projeto/src/main/java/br/felipe/ciprojeto/CiProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.felipe.ciprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiProjetoApplication.class, args);
+	}
+
+}
---
+++ ci-projeto/src/test/java/br/felipe/ciprojeto/CiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.felipe.ciprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
